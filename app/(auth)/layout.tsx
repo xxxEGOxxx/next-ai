fed13d0c5a19ccab486ef40f40a10913c5465704
EgoFox <egotdp4@gmail.com>
2023-09-14 23:01:49 +0200

    Link Clerk + some routes and navbar

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  <div className="flex items-center justify-center h-full">{children}</div>;
+};
+
+export default AuthLayout;
---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>Dashboard Page (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default DashboardPage;
