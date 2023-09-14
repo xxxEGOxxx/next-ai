d0e8a562b3da7ac55ef0b142d7c68b70bd44d6f7
EgoFox <egotdp4@gmail.com>
2023-09-14 23:01:56 +0200

    something what i miss in previous commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  <div className="flex items-center justify-center h-full">{children}</div>;
+  return (
+    <div className="flex item-center justify-center h-full">{children}</div>
+  );
 };
 
 export default AuthLayout;
