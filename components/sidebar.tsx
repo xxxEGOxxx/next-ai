464a5a17d9415858dbb462c39a50f0544fb0a758
EgoFox <egotdp4@gmail.com>
2023-09-15 14:32:45 +0200

    Improve navbar + implement dashboard

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
+
 import { cn } from "@/lib/utils";
 import {
   Code,
@@ -26,41 +28,42 @@ const routes = [
   {
     label: "Conversation",
     icon: MessageSquare,
-    href: "/dashboard",
+    href: "/conversation",
     color: "text-violet-500",
   },
   {
     label: "Image Generation",
     icon: ImageIcon,
-    href: "/dashboard",
+    href: "/image",
     color: "text-pink-700",
   },
   {
     label: "Video Generation",
     icon: VideoIcon,
-    href: "/dashboard",
+    href: "/video",
     color: "text-orange-700",
   },
   {
     label: "Music Generation",
     icon: Music,
-    href: "/dashboard",
+    href: "/music",
     color: "text-emerald-500",
   },
   {
     label: "Code Generation",
     icon: Code,
-    href: "/dashboard",
+    href: "/code",
     color: "text-green-700",
   },
   {
     label: "Settings",
     icon: Settings,
-    href: "/dashboard",
+    href: "/settings ",
   },
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -82,7 +85,12 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
