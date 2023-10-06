ba93cb8f61ff9f6379d1b12c0a57a39f639642ca
EgoFox <egotdp4@gmail.com>
2023-10-06 21:46:46 +0200

    Completed Music Generation mode

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,46 +1,38 @@
 import { Configuration, OpenAIApi } from "openai";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import Replicate from "replicate";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN!,
 });
-
-const openai = new OpenAIApi(configuration);
 
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { prompt, amount = 1, resolution = "256x256" } = body;
+    const { prompt } = body;
 
     if (!userId) {
-      return new NextResponse("Unauthorised", { status: 401 });
-    }
-
-    if (!configuration.apiKey) {
-      return new NextResponse("OpenAI API Key not configured", { status: 500 });
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
-    if (!amount) {
-      return new NextResponse("Amount is required", { status: 400 });
-    }
-    if (!resolution) {
-      return new NextResponse("Resolution is required", { status: 400 });
-    }
 
-    const response = await openai.createImage({
-      prompt,
-      n: parseInt(amount, 10),
-      size: resolution,
-    });
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      }
+    );
 
-    return NextResponse.json(response.data.data);
+    return NextResponse.json(response);
   } catch (error) {
-    console.log("[IMAGE_ERROR]", error);
-    return new NextResponse("Internal error", { status: 500 });
+    console.log("[MUSIC_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
