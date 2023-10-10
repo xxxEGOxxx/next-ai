314870db547036e7e59091365b62f74dac1ae61a
EgoFox <egotdp4@gmail.com>
2023-10-10 15:45:33 +0200

    New fix for vercel images

--- components/empty.tsx
+++ components/empty.tsx
@@ -8,7 +8,7 @@ export const Empty = ({ label }: EmptyProps) => {
   return (
     <div className="h-full p-20 flex flex-col items-center">
       <div className="relative left-6 h-52 w-72">
-        <Image alt="Empty" fill src="/empty.png" />
+        <Image alt="Empty" fill src="/static/images/empty.png" />
       </div>
       <p className=" p-7 text-muted-foreground text-md text-center">{label}</p>
     </div>
