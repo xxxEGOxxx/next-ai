2d21523ef104178320955dcd907e908915cfd521
EgoFox <egotdp4@gmail.com>
2023-10-10 15:59:10 +0200

    Just delete empty image, because vercel cant find it :/ (maybe fix in future)

--- components/empty.tsx
+++ components/empty.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Bot } from "lucide-react";
 
 interface EmptyProps {
   label: string;
@@ -7,9 +8,10 @@ interface EmptyProps {
 export const Empty = ({ label }: EmptyProps) => {
   return (
     <div className="h-full p-20 flex flex-col items-center">
-      <div className="relative left-6 h-52 w-72">
-        <Image alt="Empty" fill src="/static/images/empty.png" />
-      </div>
+      {/* <div className="relative left-[36px] h-52 w-72">
+        <Image alt="Empty" fill src="/empty.png" />
+        <Bot className="h-52 w-52" />
+      </div> */}
       <p className=" p-7 text-muted-foreground text-md text-center">{label}</p>
     </div>
   );
